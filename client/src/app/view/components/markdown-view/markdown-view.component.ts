4ffc204dfadfe37f1d97b193a20ce6c5d33fc76a
Sebastian Enns (Mac) <mail.senns@gmail.com>
2024-10-29 15:29:23 +0100

    Fixed markdown parser.

--- client/src/app/view/components/markdown-view/markdown-view.component.ts
+++ client/src/app/view/components/markdown-view/markdown-view.component.ts
@@ -1,25 +1,21 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { marked, MarkedExtension, Tokens } from 'marked';
+import { marked, RendererObject, Tokens } from 'marked';
 
 @Component({
   selector: 'app-markdown-text',
   templateUrl: './markdown-view.component.html',
 })
 export class MarkdownViewComponent implements OnChanges {
-  /**
-   * rawMarkdown: defines the raw markdown text that can be inserted into this component.
-   */
   @Input() rawMarkdown: string = '';
-
-  public htmlText: string = '';
+  public parsedHTML: string = '';
 
   constructor() {
-    const renderer: MarkedExtension['renderer'] = {
+    const renderer: RendererObject = {
       image(token: Tokens.Image): string {
-        return `</p><p class="d-block-inline text-center markdown-paragraph">
-            <img src="/assets/images/socinian/${token.href}" class="mt-4 img-fluid" alt="${token.text}" title="${token.title}" \>
-          </span>
-        </p><p class="markdown-paragraph">`;
+        return `
+            </p><p class="d-block-inline text-center markdown-paragraph">
+                <img src="/assets/images/socinian/${token.href}" class="mt-4 img-fluid" alt="${token.text}" title="${token.title}" \>
+            </p><p class="markdown-paragraph">`;
       },
 
       link(token: Tokens.Link): string {
@@ -27,7 +23,7 @@ export class MarkdownViewComponent implements OnChanges {
       },
 
       heading(token: Tokens.Heading): string {
-        const escapedText = token.text.toLowerCase().replace(/[^\w]+/g, '-');
+        const escapedText: string = token.text.toLowerCase().replace(/[^\w]+/g, '-');
 
         return `
             <h${token.depth} id="${escapedText}}" class="markdown-heading">
@@ -36,7 +32,7 @@ export class MarkdownViewComponent implements OnChanges {
       },
 
       paragraph(token: Tokens.Paragraph): string {
-        return `<p class="markdown-paragraph">${token.text}</p>`;
+        return `<p class="markdown-paragraph">${this.parser.parseInline(token.tokens)}</p>`;
       },
     };
 
@@ -45,7 +41,7 @@ export class MarkdownViewComponent implements OnChanges {
 
   public ngOnChanges(changes: SimpleChanges): void {
     if (changes['rawMarkdown']) {
-      this.htmlText = marked.parse(this.rawMarkdown) as string;
+      this.parsedHTML = marked.parse(this.rawMarkdown) as string;
     }
   }
 }
